a814697a1fe0092c5dcb5a4d94e45483f47eeb59
DazLamas <sarah.d.lamas@gmail.com>
2019-08-25 19:15:24 +0200

    Refactor. Get next stage automatically

--- app/js/classes.js
+++ app/js/classes.js
@@ -3,14 +3,14 @@
 class Task {
   constructor(name) {//TODO:Add ID
     this.name 			= name;
-    this.state			= 0;
+    this.stage			= 0;
     this.time 			= undefined;
     this.resolution 	= undefined;
     this.htmlElemet 	= `<li class="card">
 								<h4 class="card-title">${this.name}</h4>
 								<ul class="d-flex flex-row invisible">
 									<li class="card-extra-text mr-2">Time:</li>
-									<li class="card-extra-text mr-2">State</li>
+									<li class="card-extra-text mr-2">Stage</li>
 								</ul>
 							</li>`;
   }
@@ -22,22 +22,40 @@ class TasksManager {
   };
 
   createTask(name) {
-  	
+	//Create  	
   	const newTask = new Task(name);
-
+  	//Add to Queue
     this.queuedTasks.push(newTask);
-    this.addTask("queue-tasks-cont-js", newTask); //make queueEl a prop of obj
+    //Move DOM Element to Queue Column
+    this.moveToNextStage(newTask.stage, newTask.htmlElemet); //make queueEl a prop of obj
   };
   
-  addTask(where, task) {
-   document.getElementById(where).insertAdjacentHTML('beforeend', task.htmlElemet);
+  moveToNextStage(taskCurrentStage, taskHtmlNode) {
+  	// 
+  	const nextStage  = taskCurrentStage + 1;
+  	const parentNode = `stage-col-${nextStage}-js`;
+
+  	document.getElementById(parentNode).insertAdjacentHTML('beforeend', taskHtmlNode);
   };
 
   checkQueue() {
     return this.queuedTasks.size;
   };
 
+  getNextStage(task) {
+  	return task.stage + 1;
+  }
+
+  startTasks(){
+  	//check queue
+  	//Get all task an move it 
+  }
+
 };
+
+
+
+
 
 
 

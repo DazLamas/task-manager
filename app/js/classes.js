4b83f3defeb3cc358e8eb5f01bbb104a9a2e13e6
DazLamas <sarah.d.lamas@gmail.com>
2019-08-25 19:30:27 +0200

    Fix error with html stage display. Delete data-stage from dom element

--- app/js/classes.js
+++ app/js/classes.js
@@ -8,7 +8,8 @@ class Task {
     this.resolution 	= undefined;
     this.htmlElemet 	= `<li class="card">
 								<h4 class="card-title">${this.name}</h4>
-								<ul class="d-flex flex-row invisible">
+								<button id="start-progress-js" class="button">Start!</button>
+								<ul class="d-flex flex-row">
 									<li class="card-extra-text mr-2">Time:</li>
 									<li class="card-extra-text mr-2">Stage</li>
 								</ul>
@@ -27,24 +28,19 @@ class TasksManager {
   	//Add to Queue
     this.queuedTasks.push(newTask);
     //Move DOM Element to Queue Column
-    this.moveToNextStage(newTask.stage, newTask.htmlElemet); //make queueEl a prop of obj
+    this.moveToNextStage(newTask); //make queueEl a prop of obj
   };
   
-  moveToNextStage(taskCurrentStage, taskHtmlNode) {
-  	// 
-  	const nextStage  = taskCurrentStage + 1;
-  	const parentNode = `stage-col-${nextStage}-js`;
-
-  	document.getElementById(parentNode).insertAdjacentHTML('beforeend', taskHtmlNode);
+  moveToNextStage(task) {
+  	//Update task stage
+  	task.stage = task.stage + 1;
+  	//Append to next stage column
+  	document.getElementById(`stage-col-${task.stage}-js`).insertAdjacentHTML('beforeend', task.htmlElemet);
   };
 
   checkQueue() {
     return this.queuedTasks.size;
   };
-
-  getNextStage(task) {
-  	return task.stage + 1;
-  }
 
   startTasks(){
   	//check queue

475026d6e41bf7a70ac2162bfd8cf1130669caee
DazLamas <sarah.d.lamas@gmail.com>
2019-08-26 20:40:04 +0200

    Move meths to Task Object

--- app/js/classes.js
+++ app/js/classes.js
@@ -3,7 +3,7 @@
 class Task {
   constructor(id, name) {//TODO:Add ID
     this.id 			    = id;
-    this.name 			  = name;
+    this.name 			  = name || "unnamed task";
     this.stage			  = 1;
     this.duration		  = undefined;
     this.resolution 	= undefined;
@@ -23,8 +23,25 @@ class Task {
   								          <span class="card-text-box stage-text-box finished display-resolution-js"></span>
                             <button class="card-btn button btn-primary stage-text-box queue" data-js="start-progress" data-js-id="${this.id}">Start!</button>
 							           </li>`;
-  }
-}
+  };
+
+  updateStage(stage) {
+   this.stage = stage + 1;//Review sintax and change for setter
+  };
+
+  updateResolution(resolution) {//Setter
+   this.resolution = resolution;
+  };
+
+  setDuration() {
+    this.duration = generateRandomNumber(7000, 10000);
+  };
+
+  setNode(taskId) {
+    this.node = document.getElementById(`task-${taskId}`);//Setter!!!!
+  };
+
+};
 
 class TasksManager {
 
@@ -38,24 +55,17 @@ class TasksManager {
   	const newTask = new Task(generateUniqueId(), name);
   	//Add tasks to Tasks Manager array
     this.tasks.set(newTask.id, newTask);
-    //Append to next stage column
+    //Add task to queue column
     document.getElementById(`stage-col-1-js`) // Puede que esto sea mejor sacarlo a una fn "utils"
       .insertAdjacentHTML('beforeend', newTask.htmlCode); //Esto se puede sacar a una condición y realizar solo si es 0; luego se asigna id una vez incrustado y después en el else se trabaja ya con el DOMElemnt....igual así se puede actualizar el elemento html
-    newTask.node = document.getElementById(`task-${newTask.id}`);//Setter!!!!
-  };
-
-  updateStage(task) {//Método de Task, no de TM!!
-	 task.stage = task.stage + 1;//Review sintax and change for setter
-  };
-
-  updateResolution(task) {//Método de Task, no de TM!!
-	 task.stage = task.stage + 1;//Review sintax and change for setter
+    //Save DOM Element node at Task Object in order to move it throw each column
+    newTask.setNode(newTask.id);
   };
   
   moveToNextStage(task) {
 
-  	this.updateStage(task); //* también se podría hacer el update de X por separado e intentar arreglar ahí...
-  	document.getElementById(`stage-col-${task.stage}-js`).appendChild(task.node);
+  	task.updateStage(task.stage); //* también se podría hacer el update de X por separado e intentar arreglar ahí...
+  	document.getElementById(`stage-col-${task.stage}-js`).appendChild(task.node);//Utils
 
   };
 
@@ -68,18 +78,19 @@ class TasksManager {
   taskInProgress(taskId){
 
   	let task 	  = this.tasks.get(taskId);  	
-  
-  	task.duration 	= generateRandomNumber(7000, 10000);
-  	task.resolution = randomElementInArray(['success', 'failed']);
 
+    //To in progress stage
+  	this.moveToNextStage(task);
 
-  	this.moveToNextStage(task);//To in progress stage
+    task.setDuration();
+    task.updateResolution(randomElementInArray(['success', 'failed']));
 
   	setTimeout(() => {
 		  task.node.querySelector(".display-duration-js").innerText 	= Math.round(task.duration/1000);
-		  task.node.querySelector(".display-resolution-js").innerText = task.resolution;
-      task.node.querySelector(".display-resolution-js").classList.add(task.resolution);
-	  	this.moveToNextStage(task);//To finished stage
+		  task.node.querySelector(".display-resolution-js").innerText = task.resolution;////prop of task
+      task.node.querySelector(".display-resolution-js").classList.add(task.resolution);//prop of task
+	  	//To finished stage
+      this.moveToNextStage(task);
 	}, task.duration);
 
   };
--- app/js/utilities.js
+++ app/js/utilities.js
@@ -4,18 +4,6 @@ function generateUniqueId(){//TODO thisss
     return Date.now();
 };
 
-// Utilities.randomIntFromInterval(7, 10);
-var Utilities = (function utilsMethods() {
-
-    return {
-        randomIntFromInterval: function(max, min) {
-          return Math.floor(Math.random() * (max - min)) + min;
-        }
-    };
-
-}());
-
-
 function generateRandomNumber(min_value , max_value) {
     return Math.floor(Math.random() * (max_value - min_value) + min_value);;
 };
